6cdfd6c7b5f601a8caca92a89a6077694db402bb
Angel Magallanes <angelmagallanes80@gmail.com>
2022-09-07 12:58:29 +0200

    -> se crea la ruta y el archivo para la api de productos -> se configura el package json para el inicio con  "npm run dev" -> se realizan las primeras configuraciones del front en los archivos index.js e index.ejs.

--- nodeapp/routes/index.js
+++ nodeapp/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const {query, validationResult } = require('express-validator');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const ahora = new Date();
+
+  res.locals.bienvenido = 'Bienvenido a Nodepop';
+
+  res.render('index', { 
+    title: 'Nodepop',
+    diaActual: ahora.getDate(),
+    mesActual: ahora.getMonth(),
+    annoActual: ahora.getFullYear()
+  });
 });
 
 module.exports = router;
